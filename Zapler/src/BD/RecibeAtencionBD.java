309c0f3370cbb6d7a3a2cfe03042869b8cfabcee
MauricioPisconte <66890909+MauricioPisconte@users.noreply.github.com>
2021-10-06 11:06:18 -0500

    Correción del archivo subido uu

---
+++ Zapler/src/BD/RecibeAtencionBD.java
@@ -0,0 +1,40 @@
+package BD;
+import Entidades.*;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class RecibeAtencionBD {
+
+    public static String url = "jdbc:mysql://localhost:3306/zapleringsoftware";
+    public static String user = "root";
+    public static String password = "Contrasena123@";
+    public void IngresarRecibeAtencion(RecibeAtencion rban) throws Exception{
+        Connection con = DriverManager.getConnection(url, user, password);
+        String sql = "INSERT INTO RECIBE_ATENCION VALUES(?, ?)";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setInt(1, rban.getRucEmpresa());
+        pst.setInt(2, rban.getIdAtencion());
+        pst.executeUpdate();
+        pst.close();
+        con.close();
+    }
+
+    public ArrayList<RecibeAtencion> obtenerRecibeAtencion() throws Exception{
+        ArrayList<RecibeAtencion> listaRecibeAtencion = new ArrayList<>();
+        Connection con = DriverManager.getConnection(url, user, password);
+        String sql = "SELECT * FROM RECIBE_ATENCION";
+        PreparedStatement pst = con.prepareStatement(sql);
+        ResultSet rs = pst.executeQuery();
+        while(rs.next()){
+            int IdAtencion = rs.getInt("RUC_EMPRESA");
+            int RucEmpresa =  rs.getInt("ID_ATENCION");
+
+            RecibeAtencion recat = new RecibeAtencion(IdAtencion,  RucEmpresa);
+            listaRecibeAtencion.add(recat);
+        }
+        rs.close();
+        pst.close();
+        con.close();
+        return listaRecibeAtencion;
+    }
+}
---
+++ Zapler/src/Entidades/RecibeAtencion.java
@@ -0,0 +1,31 @@
+package Entidades;
+
+public class RecibeAtencion {
+    private int IdAtencion;
+    private int RucEmpresa;
+
+    public RecibeAtencion(int idAtencion, int rucEmpresa) {
+        IdAtencion = idAtencion;
+        RucEmpresa = rucEmpresa;
+    }
+    
+    public RecibeAtencion()
+    {
+    }
+
+    public int getIdAtencion() {
+        return IdAtencion;
+    }
+
+    public void setIdAtencion(int idAtencion) {
+        IdAtencion = idAtencion;
+    }
+
+    public int getRucEmpresa() {
+        return RucEmpresa;
+    }
+
+    public void setRucEmpresa(int rucEmpresa) {
+        RucEmpresa = rucEmpresa;
+    }
+}
